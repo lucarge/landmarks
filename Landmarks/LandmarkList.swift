8961ca8b2036434c92197de1297a293764725ac8
lucarge <luca.argenziano@icloud.com>
2020-07-03 14:09:34 +0200

    Add favorites

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -9,11 +9,21 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @EnvironmentObject var userData: UserData
+
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
             .navigationBarTitle("Landmarks")
@@ -24,5 +34,6 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(UserData())
     }
 }
---
+++ Landmarks/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Luca Argenziano on 03/07/2020.
+//  Copyright © 2020 Luca Argenziano. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
